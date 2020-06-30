d6de18ebabe1152118947f56036d05af2656f4af
Гаврилков Денис <Fourdi-uo@yabdex.ru>
2020-06-30 18:56:22 +0300

    1.2

--- Lab1.2/Lab1.2.py
+++ Lab1.2/Lab1.2.py
@@ -1,5 +1,18 @@
+
 from matplotlib import pyplot
-from openpyxl import load_workbook
-XL = load_workbook('data_analysis_lab.xlsx')
-TAB = XL['Data']
-TAB['A'][1:]
+from openpyxl import load_workbook as load
+
+XL = load('C:\\test\data_analysis_lab.xlsx')
+
+List = XL['Data']
+
+
+def val(x): return x.value
+
+Year = list(map(val, List['A'][1:]))
+Temp = list(map(val, List['C'][1:]))
+Active = list(map(val, List['D'][1:]))
+
+pyplot.plot(Year, Temp, label="Метка")
+pyplot.plot(Year, Active, label="Метка")
+pyplot.show() #
