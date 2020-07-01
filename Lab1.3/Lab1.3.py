63e9dea07c00e3bc5f76a182580cb8e5815ea869
Гаврилков Денис <Fourdi-uo@yabdex.ru>
2020-07-01 18:10:31 +0300

    1.2

--- Lab1.3/Lab1.3.py
+++ Lab1.3/Lab1.3.py
@@ -1,20 +1,20 @@
 from pysnmp.hlapi import *
 
 ip = "10.31.70.107"
-
+port = 161
+community = 'public'
 snmp_object = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
-
 snmp_object2 = ObjectIdentity('1.3.6.1.2.1.2.2.1.2')
 
 result = getCmd(SnmpEngine(),
-	CommunityData("public", mpModel=0),
-	UdpTransportTarget((ip, 161)),
+	CommunityData(community, mpModel=0),
+	UdpTransportTarget((ip, port)),
 	ContextData(),
 	ObjectType(snmp_object))
 
 result2 = nextCmd(SnmpEngine(),
-	CommunityData("public", mpModel=0),
-	UdpTransportTarget((ip, 161)),
+	CommunityData(community, mpModel=0),
+	UdpTransportTarget((ip, port)),
 	ContextData(),
 	ObjectType(snmp_object2),
     lexicographicMode=False)
