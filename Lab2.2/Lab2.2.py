6dd1106c7047a5f30c46d6965d2c2b97c9bdbc01
Гаврилков Денис <Fourdi-uo@yabdex.ru>
2020-07-07 13:02:36 +0300

    1.2

--- Lab2.2/Lab2.2.py
+++ Lab2.2/Lab2.2.py
@@ -3,6 +3,8 @@ import json
 import sys
 import re
 import glob
+import pprint
+import ipaddress
 
 
 tabl = {}
@@ -13,23 +15,26 @@ app = Flask(__name__)
 def index():
     web = "Бла Бла Бла Бла Бла"
     return web
-
 @app.route('/configs')
 @app.route('/configs/')
 def host():
-    hosts = []
+    hosts = str()
     for x in tabl.keys():
-        hosts.append(x)
-    return jsonify(hosts)
+        hosts = hosts+"<br>" + str(x)
+    return hosts
 
 
 @app.route('/configs/<hostname>')
 def ip(hostname):
+    v = str()
     for y in tabl.keys():
         if y == hostname:
 
-            return jsonify(tabl.get(hostname))
-    return jsonify("Not found")
+            for z in tabl.get(hostname):
+                v = v +'<br>' + str(z)
+    return v
+
+
 
 if __name__ == '__main__':
     way = glob.glob("C:\\test\config_files\*.txt")
@@ -38,14 +43,15 @@ if __name__ == '__main__':
     for file in way:
         with open(file) as newfile:
             for line in newfile:
-                IPadd = []
                 Host = re.match("^hostname (.+)", line)
                 if Host:
+
                     host = Host.group(1)
+                    IPadd = []
                 IP = re.match("^ ip address (([0-9]{1,3}\.){3}[0-9]{1,3}) (([0-9]{1,3}\.){3}[0-9]{1,3})", line)
                 if IP:
-                    IPadd.append('IP address '+"%20s" % str(IP.group(1)) + "        Mask " + "%20s" % str(IP.group(3)))
 
+                    IPadd.append(ipaddress.IPv4Interface(str(IP.group(1)) + "/" + str(IP.group(3))))
                     tabl.update({host: IPadd})
 
 
